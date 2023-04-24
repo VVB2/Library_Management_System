d607044c9443d915e00437de2dfa8a651bb9bd3d
Vinod Vaman Bhat <vinodvamanbhat@gmail.com>
2023-04-24 11:48:11 +0530

    Added book and librarian details to API

--- server/UserMicroservice/queries/IssueQueries.js
+++ server/UserMicroservice/queries/IssueQueries.js
@@ -1,4 +1,6 @@
 import issueModel from "../Models/issueModel.js";
+import booksModel from "../Models/booksModel.js";
+import librarianModel from "../Models/librarianModel.js";
 
 export const createIssueAndBookReturnNotification = async (param) => {
     await issueModel.create({
@@ -8,7 +10,18 @@ export const createIssueAndBookReturnNotification = async (param) => {
 }
 
 export const getInfo = async (param) => {
-    return await issueModel.find({ 
-        student_id: param.student_id 
+    const data = [];
+    const issueData = await issueModel.find({
+        student_id: param.student_id    
     });
+    for(const issue in issueData) {
+        const bookData = await booksModel.find({ "accession_books_list": { "$all" : [issueData[issue].accession_number] }}, { accession_books_list: 0, available_books: 0, _id: 0 });
+        const librarianData = await librarianModel.findById( issueData[issue].returned_to, { _id: 0 } );
+        data.push({
+            issueData: issueData[issue],
+            bookData,
+            librarianData
+        })
+    }
+    return data
 }
